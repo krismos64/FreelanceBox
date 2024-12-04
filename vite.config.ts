db092204ff1cf42e2b04a91d17f6a333c75226a2
krismos64 <c.mostefaoui@yahoo.fr>
2024-12-04 17:59:39 +0100

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
+      stream: "stream-browserify", // Polyfill Node.js
     },
   },
   optimizeDeps: {
-    include: ['@react-pdf/renderer']
+    include: ["@react-pdf/renderer", "stream-browserify"],
   },
   build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
     commonjsOptions: {
-      include: [/@react-pdf\/renderer/]
-    }
-  }
+      include: [/@react-pdf\/renderer/],
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
